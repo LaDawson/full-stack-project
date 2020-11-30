4b9dcd7b462446adadde862a0cc6911cd694ed84
LaDawson <ladawson@live.co.uk>
2020-11-30 19:22:24 +0000

    Edited webhook handler to fix email function

--- consultation/webhook_handler.py
+++ consultation/webhook_handler.py
@@ -3,7 +3,6 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 from .models import Consultation
-import time
 
 
 class StripeWH_Handler:
